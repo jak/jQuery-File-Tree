f98aacd82ff0865fb790e583d9f5ac26aeeb8158
Jak Spalding <jak.spalding@gmail.com>
2012-08-06 07:05:52 +0100

    work in progress

--- connectors/jqueryFileTree.php
+++ connectors/jqueryFileTree.php
@@ -4,7 +4,7 @@
 $hideDotFiles = true;
 $rootDirectory = './'; // Must have trailing slash
 
-// Define the path
+// Define the path (via get for web, or command line)
 $dir = $_GET['dir'] ? $_GET['dir'] : $argv[1];
 
 // Sanitize user input
@@ -16,6 +16,15 @@ $results = array();
 // Add a trailing slash to the directory (for string concatenation)
 if (substr($dir, -1, 1) !== DIRECTORY_SEPARATOR) {
   $dir .= DIRECTORY_SEPARATOR;
+}
+
+// Sorting function
+function filelistsort($a, $b) {
+  if ($a['dir'] == $b['dir']) {
+    return strnatcasecmp($a['name'], $b['name']);
+  }
+  if ($a['dir']) return -1;
+  return 1;
 }
 
 $dir = $rootDirectory . $dir;
@@ -30,10 +39,11 @@ if (is_dir($dir)) {
     $entry = array(
       'name' => $name,
       'dir' => is_dir($dir . $name)
-      );
+    );
 
     $results[] = $entry;
   }
+  usort($results, 'filelistsort');
   echo json_encode($results);
 } else {
   // If the parameter is not a directory, die.
